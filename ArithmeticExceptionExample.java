4b69c8e40112db8722adeb584f9b78d14d416c53
Anbarsan <anbarasan.mailme@gmail.com>
2018-08-31 10:34:34 +0530

    Finall Commit

---
+++ ArithmeticExceptionExample.java
@@ -0,0 +1,13 @@
+class ArithmeticExceptionExample{
+    public static void main(String[] args) {
+        try{
+            //Dive by Zero exception
+            int a = 100/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+        //Rest of the code can go here
+        System.out.println("Rest of the code");
+    }
+}
